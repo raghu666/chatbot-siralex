49df88dc959f4bdfa596da37f5ce29b63fe7627c
Raghu <raghu_s1990@live.com>
2024-08-05 21:02:21 -0400

    use axios to make api calls from openAI

--- src/chat_response.js
+++ src/chat_response.js
@@ -1,15 +1,23 @@
-import OpenAI from "openai";
+import axios from 'axios';
 
 async function chatResponse(messagesList, newMessage){
-    const config = { apiKey: import.meta.env.VITE_OPENAI_API_KEY, dangerouslyAllowBrowser: true };
-    const openai = new OpenAI(config);
+    let chatMessageList = [{"role": "system", "content": "You are a helpful assistant."}, ...messagesList.flat()];
 
-    let chatMessageList = [{"role": "system", "content": "You are a helpful assistant."}, ...messagesList.flat()]
-    const completion = await openai.chat.completions.create({
+    const config = { apiKey: import.meta.env.VITE_OPENAI_API_KEY };
+    const openai = axios.create({
+        baseURL: 'https://api.openai.com/v1',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${config.apiKey}`,
+        },
+    });
+
+    let completion = await openai.post('/chat/completions', {
         messages: chatMessageList,
         model: "gpt-4o-mini",
     });
-  
+    completion = completion.data;
+
     if(completion.choices[0] && completion.choices[0].message){
         newMessage.push(completion.choices[0].message);
         const index = messagesList.length - 1;
